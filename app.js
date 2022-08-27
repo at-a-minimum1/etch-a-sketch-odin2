2a977db6e91efe64d39be65d351c799ada213c63
Timothy Beler <timothybeler4@gmail.com>
2022-08-27 06:36:55 -0400

    Added functionality to the color button, on/off text to eraser and color button, moved buttons and grid size to look better, and removed the borders around the divs.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+/* @colllapse */
 let range = 16;
 let color = "black";
 let eraserState = false;
+let colorState = false;
 
 function generateDisplay(size) {
 	let display = document.querySelector(".display");
@@ -9,17 +11,14 @@ function generateDisplay(size) {
 	let squares = display.querySelectorAll("div");
 	squares.forEach((div) => div.remove());
 	let canvasSize = size * size;
+	demo.style.color = "rgb(198, 193, 30)";
 	document.getElementById("demo").innerHTML = `${size} x ${size}`;
 
 	for (let i = 0; i < canvasSize; i++) {
 		let square = document.createElement("div");
 
 		square.addEventListener("mouseover", () => {
-			if (eraserState) {
-				color = "#6A7F84";
-			} else {
-				color = "black";
-			}
+			setColor();
 			square.style.backgroundColor = color;
 		});
 		square.style.backgroundColor = "#6A7F84";
@@ -29,30 +28,52 @@ function generateDisplay(size) {
 }
 generateDisplay(range);
 
+function setColor() {
+	let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+
+	if (eraserState) {
+		color = "#6A7F84";
+	} else if (!eraserState && colorState) {
+		color = "#" + randomColor;
+	} else {
+		color = "black";
+	}
+}
+
 const clearBtn = document.querySelector(".clearBtn");
 clearBtn.addEventListener("click", () => {
 	generateDisplay(range);
 });
 let colorBtn = document.querySelector(".colorBtn");
 colorBtn.addEventListener("click", () => {
-	console.log("This works");
+	if (!colorState) {
+		colorBtn.innerHTML = "On";
+		// colorBtn.style.backgroundColor = "grey";
+		colorBtn.style.color = "black";
+		colorState = true;
+	} else {
+		colorBtn.innerHTML = "Off";
+		// colorBtn.style.backgroundColor = "#D9D9D9";
+		colorBtn.style.color = "black";
+		colorState = false;
+	}
 });
 let eraserBtn = document.querySelector(".eraserBtn");
 eraserBtn.addEventListener("click", () => {
-	console.log("This works");
 	if (!eraserState) {
-		eraserBtn.style.backgroundColor = "pink";
+		eraserBtn.innerHTML = "On";
+		// eraserBtn.style.backgroundColor = "#79090F";
 		eraserBtn.style.color = "black";
 		eraserState = true;
 	} else {
-		eraserBtn.style.backgroundColor = "black";
-		eraserBtn.style.color = "white";
+		eraserBtn.innerHTML = "Off";
+		// eraserBtn.style.backgroundColor = "#D9D9D9";
+		eraserBtn.style.color = "black";
 		eraserState = false;
 	}
 });
 
 function sliderFunction(slideNum) {
-	// document.getElementById("demo").innerHTML = `${slideNum} x ${slideNum}`;
 	range = slideNum;
 	generateDisplay(slideNum);
 }
